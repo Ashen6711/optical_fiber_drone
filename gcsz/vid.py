d9dd5e8061b05866e7fc5aed012a3af752d0bc5e
Ashen6711 <sagar.tolle@gmail.com>
2026-04-11 17:49:58 +0530

    Refresh

--- gcsz/vid.py
+++ gcsz/vid.py
@@ -1,18 +1,24 @@
 import socket
 import struct
 import io
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import cv2
 import numpy as np
 import time
+import random
 
 PC_PORT = 14550
+
+FONT_PATH = r"C:\Users\Hi\Downloads\uav_osd\UAV-OSD-Mono.ttf"
+FONT = ImageFont.truetype(FONT_PATH, size=10)
 
 fragments = {}
 frame_stats = {}
 wait = 0
 frames_no = 0
 frames_ = True
+
+jitter_frame = 0
 
 fps_timer = time.time()
 fps = 9.4
@@ -23,6 +29,28 @@ sock.settimeout(1.0)
 
 print(f"Listening for video on port {PC_PORT}")
 
+def draw_text(frame, text, pos, color, opacity):
+    global jitter_frame
+    jitter_frame += 1
+
+    img_pil = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)).convert("RGBA")
+    overlay = Image.new("RGBA", img_pil.size, (0, 0, 0, 0))
+    draw = ImageDraw.Draw(overlay)
+
+    x, y = pos
+    r, g, b = color
+
+    jx = random.randint(-2, 2) if jitter_frame % 10 == 0 else 0
+    jy = random.randint(-1, 1) if jitter_frame % 7 == 0 else 0
+
+    draw.text((x + jx - 2, y + jy), text, font=FONT, fill=(r, 0, 0, opacity))   
+    draw.text((x + jx, y + jy), text, font=FONT, fill=(0, g, 0, opacity))   
+    draw.text((x + jx + 2, y + jy), text, font=FONT, fill=(0, 0, b, opacity))   
+    draw.text((x + jx, y + jy), text, font=FONT, fill=(r,g,b, opacity))
+
+    img_pil = Image.alpha_composite(img_pil, overlay)
+    return cv2.cvtColor(np.array(img_pil.convert("RGB")), cv2.COLOR_RGB2BGR)
+
 def process_frame(frame_id, fragments_list):
     try:
         data = b''.join([f[1] for f in sorted(fragments_list)])
@@ -30,7 +58,7 @@ def process_frame(frame_id, fragments_list):
         image = Image.open(io.BytesIO(data))
         frame = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
-        cv2.putText(frame, f"FPS: {fps:.1f}", (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        frame = draw_text(frame, f"FPS: {fps:.1f}", pos=(10, 5), color=(255, 255, 255), opacity = 180)
         cv2.imshow("ESP32-CAM", frame)
         cv2.waitKey(1)
 
